d5b110e75bf05d4a984eca36e34ec0ce506344f3
Rikcher <nick.richardson.4884@gmail.com>
2025-01-05 14:54:27 +0700

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+import React from "react";
+
+const font = Open_Sans({subsets: ['latin']})
+
+export const metadata: Metadata = {
+  title: "Chatter Space",
+  description: "Chatter Space Application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${font.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
