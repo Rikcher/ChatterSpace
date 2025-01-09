673d11195b49ef72da99fb0648f7812a3dc3f306
Rikcher <nick.richardson.4884@gmail.com>
2025-01-09 16:54:24 +0700

    fix: Delete test button

--- app/actions.ts
+++ app/actions.ts
@@ -4,31 +4,6 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { createClient, getURL } from '@/shared/lib/utils';
 
-export async function signup(formData: FormData) {
-  const supabase = await createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-    options: {
-      data: {
-        username: formData.get('username') as string,
-      },
-    },
-  };
-
-  const { error } = await supabase.auth.signUp(data);
-
-  if (error) {
-    redirect('/error');
-  }
-
-  revalidatePath('/', 'layout');
-  redirect('/private');
-}
-
 export async function signOut() {
   const supabase = await createClient();
   await supabase.auth.signOut();
