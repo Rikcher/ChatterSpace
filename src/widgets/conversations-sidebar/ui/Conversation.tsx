53d4565d369f4fe64d26e95f677f816d32dfcc62
Rikcher <nick.richardson.4884@gmail.com>
2025-01-22 00:53:14 +0700

    feat: Create styles to indicate active conversation

--- src/widgets/conversations-sidebar/ui/Conversation.tsx
+++ src/widgets/conversations-sidebar/ui/Conversation.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/shared/lib/utils';
 import { UserAvatar } from '@/entities/user';
 import { Button } from '@/shared/shadcn-ui';
 import { Profile } from '@prisma/client';
-import { useRouter } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 
 interface ConversationProps {
   profile: Profile;
@@ -13,6 +13,7 @@ interface ConversationProps {
 
 const Conversation: React.FC<ConversationProps> = ({ profile }) => {
   const router = useRouter();
+  const params = useParams();
 
   const onClick = () => {
     router.push(`/conversations/${profile.id}`);
@@ -22,7 +23,10 @@ const Conversation: React.FC<ConversationProps> = ({ profile }) => {
     <Button
       onClick={onClick}
       className={cn(
-        'group px-2 py-2 mb-2 rounded-md flex items-center gap-2 w-full bg-transparent text-foreground/60 justify-start hover:bg-foreground/5'
+        'group px-2 py-2 mb-2 rounded-md flex items-center gap-2 w-full bg-transparent text-foreground/60 justify-start hover:bg-foreground/5',
+        profile.id === params.profileId
+          ? 'bg-foreground/20 text-foreground hover:bg-foreground/20'
+          : 'hover:bg-foreground/5'
       )}
     >
       <UserAvatar
@@ -32,7 +36,10 @@ const Conversation: React.FC<ConversationProps> = ({ profile }) => {
       />
       <p
         className={cn(
-          'line-clamp-1 font-semibold text-xs text-foreground/60 group-hover:text-foreground transition-colors'
+          'line-clamp-1 font-semibold text-xs text-foreground/60 group-hover:text-foreground transition-colors',
+          profile.id === params.profileId
+            ? 'text-foreground'
+            : 'group-hover:text-foreground/80'
         )}
       >
         {profile.username}
